6e0631f2bb7ee2eaa5936ddadf10b4a3d2876db4
Majkel Kokocinski <majkelkokocinski@gmail.com>
2024-02-16 11:06:01 +0100

    Starten för nya Schackpussel-sidan

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import './assets/main.css'
+import './assets/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Importera bootstrap CSS stilar
+import 'bootstrap/dist/css/bootstrap.css'
+
+// Importera bootstrap JS filer
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +15,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// För att importer ska fungera behöver man använda ett middleware "use"
+app.use(bootstrap)
 
 app.mount('#app')
