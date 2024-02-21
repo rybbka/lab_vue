beaecd4d69382574ca15865bcd50789b25d102b0
Majkel Kokocinski <majkelkokocinski@gmail.com>
2024-02-21 23:38:23 +0100

    Inlämningsversionen

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="about">
-
-    <h1>(CP) is a relentless quest to unearth the finest chess puzzles, offering them to you, our fellow aficionados, free of charge, 4ever. </h1>
+    <h1>(CP) is on a relentless quest to unearth the world's finest chess puzzles, offering them to you, our fellow aficionados, free of charge, 4-ever.</h1>
+    <!-- Lägg till din bild här under h1 -->
+    <img src="@/assets/img/chesspieces_exp.png" alt="Chess pieces explained" class="about-image"/>
   </div>
 </template>
 
 <style scoped>
-@media (min-width: 340px) {
+@media (min-width: 300px) {
   .about {
     min-height: 100vh;
     display: flex;
-    align-items: center;
+    flex-direction: column; /* Ändra till column för att ordna innehållet vertikalt */
+    align-items: center; /* Centrerar innehållet horisontellt */
+    justify-content: center; /* Centrerar innehållet vertikalt */
     background-color: #42210B;
     padding: 0;
-
   }
 
   .brand {
@@ -23,16 +25,20 @@
   h1 {
     font-size: 2em;
     color: #FFFC47;
-    padding-left: 3em;
-    padding-right: 3em;
-    padding-top: 0;
+    padding: 1em; /* Justerade vaddering för att ge utrymme ovan och nedan om texten */
+  }
 
+  /* Stil för din bild */
+  .about-image {
+    max-width: 40%; /* Gör att bilden inte överskrider containerbredden */
+    height: auto; /* Bibehåller bildens aspektförhållande */
+    padding: 1em; /* Lägg till lite utrymme runt bilden */
+    padding-bottom: 10em;
   }
 
   body {
     padding-top: 2em;
-    padding-bottom: 2em;
+    padding-bottom: 1em;
   }
-
 }
 </style>
